97c2e00ef0cc97a5d0102473c1abfaece855beee
Javier <you@example.com>
2023-10-18 12:17:46 -0300

    Step-1

---
+++ app/src/main/java/com/example/horoscopoapp/ui/home/luck/LuckFragment.kt
@@ -0,0 +1,23 @@
+package com.example.horoscopoapp.ui.home.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscopoapp.R
+import com.example.horoscopoapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
