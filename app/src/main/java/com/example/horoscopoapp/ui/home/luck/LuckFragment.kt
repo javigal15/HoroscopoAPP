38e4c59867b889ae62b8cf4cdc97eef9e8133311
Javier <you@example.com>
2023-10-20 14:53:17 -0300

    Step 8 - LuckFragment Animation complete

--- app/src/main/java/com/example/horoscopoapp/ui/home/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/home/luck/LuckFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
@@ -61,7 +62,6 @@ class LuckFragment : Fragment() {
             }
 
             override fun onAnimationRepeat(p0: Animation?) {}
-
         })
         binding.reverse.startAnimation(slideUpAnimation)
     }
@@ -71,13 +71,35 @@ class LuckFragment : Fragment() {
 
         growAnimation.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(p0: Animation?) {}
-
-            override fun onAnimationEnd(p0: Animation?) {}
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.reverse.isVisible = false
+                showPremonitionView()
+            }
 
             override fun onAnimationRepeat(p0: Animation?) {}
-
         })
         binding.reverse.startAnimation(growAnimation)
+    }
+
+    private fun showPremonitionView() {
+        val disappearAnimation = AlphaAnimation(1.0f, 0.0f) //Cambia opacidad
+        disappearAnimation.duration = 200
+
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+        appearAnimation.duration = 1000
+
+        disappearAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {}
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.preview.isVisible = false
+                binding.prediction.isVisible = true
+
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+        })
+        binding.preview.startAnimation(disappearAnimation)
+        binding.prediction.startAnimation(appearAnimation)
     }
 
     override fun onCreateView(
